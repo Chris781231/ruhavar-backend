6a492b62c9b78ed66366979c885f4aa43baf9526
LigetiKaroly <ligeti.karoly78@gmail.com>
2021-07-31 15:36:14 +0200

    Init commit.

---
+++ src/main/java/ruhavarbackend/RuhavarBackendApplication.java
@@ -0,0 +1,13 @@
+package ruhavarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuhavarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuhavarBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ruhavarbackend/RuhavarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ruhavarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuhavarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
